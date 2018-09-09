26fd12dfffcb967a5eb5be90f0e90b5270e57209
ilnar4888 <ilnar4888@gmail.com>
2018-09-09 16:42:18 +0300

    new gem

--- ilnar_nmax.gemspec
+++ ilnar_nmax.gemspec
@@ -3,8 +3,8 @@ require File.expand_path('../lib/ilnar_nmax/version', __FILE__)
 Gem::Specification.new do |gem|
   gem.authors       = ["ilnar4888"]
   gem.email         = ["ilnar4888@gmail.com"]
-  gem.description   = %q{nkjnjknjjn}
-  gem.summary       = %q{hjbhbhjbhjbhjb}
+  gem.description   = %q{}
+  gem.summary       = %q{}
   gem.homepage      = ""
 
   gem.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
--- lib/ilnar_nmax.rb
+++ lib/ilnar_nmax.rb
@@ -1,7 +1,11 @@
 require "ilnar_nmax/version"
 
 module IlnarNmax
-  def self.nmax(n)
-    n.scan(/[0-9]{0,1000}/).map{|i| i.to_i}.max
-  end
+	def self.nmax(file = ARGV[0], numbers = ARGV[1])
+	  numbers = []  #создаём массив
+	  file.each_line do |line| #
+	    line.scan(/[0-9]{1,1000}/).each{|i| numbers << i.to_i} # фильтруем числа из текста и кладём в массив преобразовав в integer
+	  end
+	  numbers.uniq.sort.reverse[0...n] #уникальность, сортировка, упорядочение по убыванию, получение из диапазона до "n"
+	end
 end
